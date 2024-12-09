f46a32feb75c8462bbb3d7c722a66a5d323df2c5
Kerteszmarton2 <kermar709@hengersor.hu>
2024-12-09 14:47:48 +0100

    reactfilozofia

---
+++ React/reactfilozofia/app.js
@@ -0,0 +1,21 @@
+const btn =document.createElement('button');
+btn.onclick=function(){
+    alert("Ez egy natív kód");
+}
+btn.innerHTML="Natív gomb";
+document.getElementById("native-button-container").appendChild(btn);
+
+
+
+const gomb =React.createElement(
+    'button',
+    {
+    onClick: function() {
+            alert('Klikk esemény történt');
+        },
+    
+    },
+"Gomb 2"
+);
+
+ReactDOM.render(gomb, document.getElementById('react-button-container'));
