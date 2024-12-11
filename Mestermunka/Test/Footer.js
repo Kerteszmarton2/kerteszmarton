7a4d7ebc017345793962c6550cfd47cba9ead82b
Kerteszmarton2 <kermar709@hengersor.hu>
2024-12-11 10:00:50 +0100

    TEST
    full AI

---
+++ Mestermunka/Test/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="bg-light text-center py-3">
+      <p>&copy; 2023 Perfume Store. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
