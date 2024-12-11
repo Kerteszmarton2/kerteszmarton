15f7c2717d5aec5ba888ee490dcca54a20db3c9b
Kerteszmarton2 <kermar709@hengersor.hu>
2024-12-11 13:09:29 +0100

    12.11
    Megjegyzésekkel

--- React/komponens/app.js
+++ React/komponens/app.js
@@ -1,30 +1,30 @@
-function App(){
-    return React.createElement(
-        "div",
+function App(){               // App function eleje
+    return React.createElement(                                     //Visszaad és létrehoz egy elemet
+        "div",                                                      //Az elemen belül megjelenít egy div-et
         {
-            className:"border"
+            className:"border"                                      //Add a divnek egy border nevű classt
         },
         "Doboz",
-        React.createElement(BoxComponent),
+        React.createElement(BoxComponent),                          //Létrehozza ide a box komponenst
     )}
 
-    function BoxComponent(props){
-        
-        return React.createElement(
-            "div",
+    function BoxComponent(props){                                   //box komponens eleje props értékkel.
+
+        return React.createElement(                                 //Visszaad és létrehoz egy elemet
+            "div",                                                  //Az elemen belül megjelenít egy div-et
      
             {
      
-                style:{
+                style:{                                             //Stílust adunk meg a divnek
                     width:"200px",
                     height:"200px",
                     backgroundColor:"red",
      
                 },
-                className:"p-2 m-5 rounded",
+                className:"p-2 m-5 rounded",                         //Classt is adunka divnek
             }
         )}
-ReactDOM.render (React.createElement(App),  document.getElementById("app-container"));
+ReactDOM.render (React.createElement(App),  document.getElementById("app-container")); //Rendereli az app function tartalmát a html "app-container" id-je  helyére.
             
           
          
